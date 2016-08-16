8cbd2cd1df8d33862f9ce0610517bca53ec44e81
Rei <chloerei@gmail.com>
2016-08-17 00:39:04 +0800

    Install Turbolinks and implements base TurbolinksAdapter

--- app/src/main/java/org/ruby_china/rubychina/MainActivity.java
+++ app/src/main/java/org/ruby_china/rubychina/MainActivity.java
@@ -1,13 +1,81 @@
 package org.ruby_china.rubychina;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.basecamp.turbolinks.TurbolinksAdapter;
+import com.basecamp.turbolinks.TurbolinksSession;
+import com.basecamp.turbolinks.TurbolinksView;
+
+public class MainActivity extends AppCompatActivity implements TurbolinksAdapter {
+
+    private static final String BASE_URL = "https://ruby-china.org";
+    private static final String INTENT_URL = "intentUrl";
+
+    private String location;
+    private TurbolinksView turbolinksView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        turbolinksView = (TurbolinksView) findViewById(R.id.turbolinks_view);
+
+        TurbolinksSession.getDefault(this).setDebugLoggingEnabled(true);
+
+        location = getIntent().getStringExtra(INTENT_URL) != null ? getIntent().getStringExtra(INTENT_URL) : BASE_URL;
+
+        TurbolinksSession.getDefault(this)
+                         .activity(this)
+                         .adapter(this)
+                         .view(turbolinksView)
+                         .visit(location);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        TurbolinksSession.getDefault(this)
+                .activity(this)
+                .adapter(this)
+                .restoreWithCachedSnapshot(true)
+                .view(turbolinksView)
+                .visit(location);
+    }
+
+    @Override
+    public void onPageFinished() {
+
+    }
+
+    @Override
+    public void visitProposedToLocationWithAction(String location, String action) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra(INTENT_URL, location);
+
+        this.startActivity(intent);
+    }
+
+    @Override
+    public void onReceivedError(int errorCode) {
+
+    }
+
+    @Override
+    public void pageInvalidated() {
+
+    }
+
+    @Override
+    public void requestFailedWithStatusCode(int statusCode) {
+
+    }
+
+    @Override
+    public void visitCompleted() {
+
     }
 }
