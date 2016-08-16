85b194b1eef80ccf61e2695b5a479bbf827c301a
Rei <chloerei@gmail.com>
2016-08-16 23:58:28 +0800

    Init commit

---
+++ app/src/main/java/org/ruby_china/rubychina/MainActivity.java
@@ -0,0 +1,13 @@
+package org.ruby_china.rubychina;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
