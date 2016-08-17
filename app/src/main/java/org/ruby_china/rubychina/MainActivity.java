7f2c4655843d04ab3c47ecfac293c0d0ff159aed
Rei <chloerei@gmail.com>
2016-08-18 01:37:25 +0800

    Add toolbar

--- app/src/main/java/org/ruby_china/rubychina/MainActivity.java
+++ app/src/main/java/org/ruby_china/rubychina/MainActivity.java
@@ -3,6 +3,7 @@ package org.ruby_china.rubychina;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.webkit.WebSettings;
 
 import com.basecamp.turbolinks.TurbolinksAdapter;
@@ -21,6 +22,9 @@ public class MainActivity extends AppCompatActivity implements TurbolinksAdapter
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(myToolbar);
 
         turbolinksView = (TurbolinksView) findViewById(R.id.turbolinks_view);
 
